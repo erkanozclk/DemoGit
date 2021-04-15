1b1eb940034a96afa4c4212e7e3d4f006a292eeb
ErkanOzclk <68093393+erkanozclk@users.noreply.github.com>
2021-04-15 18:41:55 -0400

    Update DemoClass.java

--- src/test/java/demo/DemoClass.java
+++ src/test/java/demo/DemoClass.java
@@ -5,6 +5,7 @@ public class DemoClass {
     public static void main(String[] args) {
         System.out.println("First commit");
         System.out.println("second commit");
+        System.out.println("third commit");
 
     }
 
