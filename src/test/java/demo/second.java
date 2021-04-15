0422b29c5fb5391f1c18461d3e91dce8908c2733
erkanozcelik19@gmail.com <Karpuz.47>
2021-04-15 18:52:31 -0400

    fourt commit

---
+++ src/test/java/demo/second.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class second {
+
+    public static void main(String[] args) {
+        System.out.println("First commit");
+        System.out.println("second commit");
+    }
+}
