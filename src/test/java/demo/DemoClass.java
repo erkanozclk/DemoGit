f53fc2904f2848b741415dafc9afa26fbee664c2
erkanozcelik19@gmail.com <Karpuz.47>
2021-04-15 18:42:43 -0400

    fourt commit

--- src/test/java/demo/DemoClass.java
+++ src/test/java/demo/DemoClass.java
@@ -5,7 +5,7 @@ public class DemoClass {
     public static void main(String[] args) {
         System.out.println("First commit");
         System.out.println("second commit");
-
+        System.out.println("forth commit");
     }
 
 
